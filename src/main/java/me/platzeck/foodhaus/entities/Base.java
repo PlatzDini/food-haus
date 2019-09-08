3d58b641fa7250fddbe2c81fc6777d30f0adce64
Eduardo Dini <eduardodiasdini@gmail.com>
2019-09-08 15:37:42 -0300

    Criando ingredient entity

---
+++ src/main/java/me/platzeck/foodhaus/entities/Base.java
@@ -0,0 +1,37 @@
+package me.platzeck.foodhaus.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class Base {
+
+    @Id
+    private UUID id;
+
+    @Column(name="created_at", columnDefinition = "timestamptz")
+    private OffsetDateTime createdAt;
+    @Column(name="updated_at", columnDefinition = "timestamptz")
+    private OffsetDateTime updatedAt;
+    private boolean removed;
+
+    @PrePersist
+    public void prePersist(){
+        this.id = UUID.randomUUID();
+        this.createdAt = OffsetDateTime.now();
+        this.removed = false;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        this.updatedAt = OffsetDateTime.now();
+    }
+
+
+}
