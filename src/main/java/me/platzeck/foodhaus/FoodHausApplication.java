8dbe9aee32b30092037f58f20d083b832b70c2fe
Guilherme Platzeck <gplatzeck@benomad.us>
2019-09-08 14:07:24 -0300

    init repo

---
+++ src/main/java/me/platzeck/foodhaus/FoodHausApplication.java
@@ -0,0 +1,13 @@
+package me.platzeck.foodhaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodHausApplication.class, args);
+	}
+
+}
